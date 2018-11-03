a6444ac60dc20fb42be827ae5a5fecca80f22664
Martin L. Jensen <martinloesethjensen@gmail.com>
2018-11-03 16:14:05 +0100

    Create case

--- src/main/java/com/hackathon/controller/CaseController.java
+++ src/main/java/com/hackathon/controller/CaseController.java
@@ -1,9 +1,16 @@
 package com.hackathon.controller;
 
 import com.hackathon.model.Case;
+import com.hackathon.model.ModelFiles;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Controller
 public class CaseController {
 
 	@GetMapping("/cases")
@@ -14,7 +21,17 @@ public class CaseController {
 	@GetMapping("/create_case")
 	public String createCase(Model model){
 		model.addAttribute("case", new Case());
-
 		return "create_case";
+	}
+
+	@PostMapping("/create_case")
+	public String saveCase(Case c) throws IOException, ClassNotFoundException {
+		ArrayList cases = ModelFiles.load("C:\\Users\\marti\\Dropbox\\KEA - Datamatiker\\GitHub\\hackathonAdvokat\\src\\main\\resources\\files\\cases.txt");
+		cases.add(c);
+		System.out.println(cases);
+
+//		ModelFiles.save("C:\\Users\\marti\\Dropbox\\KEA - Datamatiker\\GitHub\\hackathonAdvokat\\src\\main\\resources\\files\\cases.txt", cases);
+
+		return "redirect:/cases";
 	}
 }
